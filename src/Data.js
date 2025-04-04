c7d10b1a4d20be21ccaeb574c64d0eba69b1b0bd
jeetendrakumar01 <jeetendrakumar5861@gmail.com>
2025-04-04 09:13:54 +0530

    stylees

--- src/Data.js
+++ src/Data.js
@@ -1,46 +1,30 @@
 import React, { useState } from 'react';
 import datas from './datas.json';
 import './styles.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCookie, faCoffee, faIceCream } from '@fortawesome/free-solid-svg-icons';
 
 const Data = () => {
     const [data, setData] = useState(datas);
 
     const Remove = (itemId) => {
-        setData(data.filter(item => item.
-            id!== itemId))
+        console.log("Removing ID:", itemId);
+        setData(prevData => prevData.filter(item => item.id !== itemId));
     }
-
+    
     const Update = (itemId) => {
-        setData(data.map(items => {
-            if (items.id === itemId) {
-                return {type: "Updated Type" };
-            } else {
-                return items;
-            }
-        }));
+        console.log("Updating ID:", itemId);
+        setData(prevData =>
+            prevData.map(item =>
+                item.id === itemId ? { ...item, type: "Updated Type" } : item
+            )
+        );
     }
-
-    const getIcon = (type) => {
-        switch (type) {
-            case 'Glazed':
-                return <FontAwesomeIcon icon={faCookie} />;
-            case 'Sugar':
-                return <FontAwesomeIcon icon={faCoffee} />;
-            case 'Powdered Sugar':
-                return <FontAwesomeIcon icon={faIceCream} />;
-            default:
-                return null;
-        }
-    }
-
-    return (
+    
+    
+   return (
         <div className="data-container">
             {data.map(item => (
                 <div className="card" key={item.id}>
                     <div className="card-content">
-                        {getIcon(item.type)}
                         <h3>{item.type}</h3>
                         <button onClick={() => Remove(item.id)}>Remove</button>
                         <button onClick={() => Update(item.id)}>Update</button>
